36b9f36843c823a30761069959e588a5fc07ca56
Phillip <phillip-shields@live.nmit.ac.nz>
2019-12-14 21:08:42 +1300

    successful api connection
    next work on a display

--- src/InputForm.svelte
+++ src/InputForm.svelte
@@ -1,29 +1,56 @@
 <script>
+  import { createEventDispatcher } from "svelte";
 
-let loverOne, loverTwo;
+  const dispatch = createEventDispatcher();
 
+  const apiBaseUrl = "https://love-calculator.p.rapidapi.com/getPercentage?";
+  // fname=&sname=
 
-const onSubmit = e => {
+  let love, loverOne, loverTwo;
+
+  async function onSubmit(e) {
     e.preventDefault();
-    console.log('submitted');
-} 
+    const res = await fetch(
+      apiBaseUrl + "fname=" + loverOne + "&sname=" + loverTwo,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+          "x-rapidapi-key": "38fcf7fcfamshc75632e3ce098bdp1db136jsn816d2fbc625e"
+        }
+      }
+    );
+    love = await res.json();
+    console.dir(love);
+  }
 </script>
 
+<form
+  on:submit={onSubmit}
+  class="w-full text-red-700 px-4 text-center text-xl md:text-3xl">
 
-<form on:submit={onSubmit} class="w-full text-red-700 px-4 text-center text-xl md:text-3xl">
-
-<div class="w-full py-8">
+  <div class="w-full py-8">
     <!-- first name input -->
     <div class="w-full md:w-1/2 mx-auto">
-        <label for="lover one">First Lovers Name </label>
-        <input type="text" bind:value={loverOne} placeholder="Lovuh One" class="appearance-none border-b-4 border-red-700">
+      <label for="lover one">First Lovers Name</label>
+      <input
+        type="text"
+        bind:value={loverOne}
+        placeholder="Lovuh One"
+        class="appearance-none border-b-4 border-red-700" />
     </div>
     <!-- second name input -->
     <div class="w-full md:w-1/2 mx-auto">
-        <label for="lover two">Second Lovers Name </label>
-        <input type="text" bind:value={loverOne} placeholder="Lovuh Two" class="appearance-none border-b-4 border-red-700">
+      <label for="lover two">Second Lovers Name</label>
+      <input
+        type="text"
+        bind:value={loverTwo}
+        placeholder="Lovuh Two"
+        class="appearance-none border-b-4 border-red-700" />
     </div>
-</div>
-<button type="submit" class="px-4 py-1 rounded-full bg-red-700 text-white">submit</button>
+  </div>
+  <button type="submit" class="px-4 py-1 rounded-full bg-red-700 text-white">
+    submit
+  </button>
 
 </form>
